3e94ce1b85d36d80ebb5bf30f296f2505e878b49
rthor <rthor@LAPTOP-343NT6OO>
2016-09-21 16:39:13 -0700

    Project demonstrates migration to database.

---
+++ src/ToDoListWithMigrations/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListWithMigrations.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public int CategoryId {get; set;}
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ src/ToDoListWithMigrations/Models/ToDoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoListWithMigrations.Models
+{
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
